6a7cc7eff3547ad02b53f7314172278d69a62b6c
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-07-21 14:04:52 +0300

    Fix artifact deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,10 @@ plugins {
     id("com.jfrog.bintray") version "1.8.4"
 }
 
+val artifactVersion = "0.2"
+
 group = "org.knanoid"
-version = "0.1"
+version = artifactVersion
 
 repositories {
     mavenCentral()
@@ -34,6 +36,23 @@ tasks {
     }
 }
 
+val pomUrl = "https://github.com/alekseinovikov/knanoid"
+val pomScmUrl = "https://github.com/alekseinovikov/knanoid"
+val pomIssueUrl = "https://github.com/alekseinovikov/knanoid/issues"
+val pomDesc = "https://github.com/alekseinovikov/knanoid"
+
+val gitHubRepo = "https://github.com/alekseinovikov/knanoid.git"
+val githubReadme = "README.md"
+
+val pomLicenseName = "GPL3"
+val pomLicenseUrl = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+val pomLicenseDist = "repo"
+
+val pomDeveloperId = "alekseinovikov"
+val pomDeveloperName = "Aleksei Novikov"
+
+val toolDescription = "KNanoid generates unique IDs"
+
 publishing {
     publications {
         create<MavenPublication>("maven-publication") {
@@ -44,9 +63,21 @@ publishing {
 
             pom.withXml {
                 asNode().apply {
-                    appendNode("description", "KNanoid generates unique IDs")
+                    appendNode("description", toolDescription)
                     appendNode("name", "KNanoid")
-                    appendNode("url", "https://github.com/alekseinovikov/knanoid")
+                    appendNode("url", pomUrl)
+                    appendNode("licenses").appendNode("license").apply {
+                        appendNode("name", pomLicenseName)
+                        appendNode("url", pomLicenseUrl)
+                        appendNode("distribution", pomLicenseDist)
+                    }
+                    appendNode("developers").appendNode("developer").apply {
+                        appendNode("id", pomDeveloperId)
+                        appendNode("name", pomDeveloperName)
+                    }
+                    appendNode("scm").apply {
+                        appendNode("url", pomScmUrl)
+                    }
                 }
             }
         }
@@ -65,5 +96,26 @@ bintray {
         name = project.name
         setLicenses("GPL-3.0")
         vcsUrl = "https://github.com/alekseinovikov/knanoid.git"
+    }
+
+    pkg.apply {
+        repo = "maven"
+        name = "knanoid"
+        userOrg = pomDeveloperId
+        githubRepo = gitHubRepo
+        vcsUrl = pomScmUrl
+        description = toolDescription
+        setLabels("kotlin", "knanoid")
+        setLicenses("GRPL-3.0")
+        desc = toolDescription
+        websiteUrl = pomUrl
+        issueTrackerUrl = pomIssueUrl
+        githubReleaseNotesFile = githubReadme
+
+        version.apply {
+            name = artifactVersion
+            desc = pomDesc
+            vcsTag = artifactVersion
+        }
     }
 }
