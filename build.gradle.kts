8d6eae562a3973cec53809eb91ec67ddf9cd2046
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-07-21 13:34:14 +0300

    Added publishing into bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
     kotlin("jvm") version "1.3.72"
+    id("maven-publish")
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "org.knanoid"
@@ -29,5 +31,39 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven-publication") {
+            groupId = "org.knanoid"
+            artifactId = "knanoid"
+
+            from(components["java"])
+
+            pom.withXml {
+                asNode().apply {
+                    appendNode("description", "KNanoid generates unique IDs")
+                    appendNode("name", "KNanoid")
+                    appendNode("url", "https://github.com/alekseinovikov/knanoid")
+                }
+            }
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER") ?: System.getProperty("bintrayUser")
+    key = System.getenv("BINTRAY_KEY") ?: System.getProperty("bintrayKey")
+
+    setConfigurations("archives")
+    setPublications("mavenPublication")
+
+    pkg.apply {
+        repo = "utils"
+        name = project.name
+        setLicenses("GPL-3.0")
+        vcsUrl = "https://github.com/alekseinovikov/knanoid.git"
     }
 }
