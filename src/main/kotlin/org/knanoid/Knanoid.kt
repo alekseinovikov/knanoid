00352808b1766a649bfc9b4c24b89b75fd4c8fa0
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2020-07-21 12:15:04 +0300

    Refactored API and wrote additional tests for argument validation and distribution

--- src/main/kotlin/org/knanoid/Knanoid.kt
+++ src/main/kotlin/org/knanoid/Knanoid.kt
@@ -1,22 +1,24 @@
 package org.knanoid
 
-import java.security.SecureRandom
-import java.util.*
 import kotlin.math.ceil
 import kotlin.math.floor
 import kotlin.math.ln
+import kotlin.random.Random
 
-private val generator = SecureRandom()
-private val alphabet = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
+private val defaultRandom = Random
+private val defaultAlphabet = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
 
 private const val defaultSize = 21
 
-private fun randomNanoId(random: Random, chars: CharArray, size: Int): String {
-    if (chars.isEmpty() || chars.size >= 256) throw IllegalArgumentException("chars must be in range (0..256)")
-    if (size <= 0) throw IllegalArgumentException("size must be more than zero")
+fun randomNanoId(
+    size: Int = defaultSize,
+    alphabet: CharArray = defaultAlphabet,
+    random: Random = defaultRandom
+): String {
+    validateArguments(alphabet, size)
 
-    val mask = (2 shl (floor(ln((chars.size - 1).toDouble()) / ln(2.0))).toInt()) - 1
-    val step = ceil(1.6 * mask * size / chars.size).toInt()
+    val mask = calculateMask(alphabet)
+    val step = calculateStep(mask, size, alphabet)
 
     val resultStringBuilder = StringBuilder()
 
@@ -26,16 +28,24 @@ private fun randomNanoId(random: Random, chars: CharArray, size: Int): String {
 
         for (i in 0 until step) {
             val index = bytes[i].toInt() and mask
-            if (index < chars.size) {
-                resultStringBuilder.append(chars[index])
+            if (index < alphabet.size) {
+                resultStringBuilder.append(alphabet[index])
 
                 if (size == resultStringBuilder.length) {
                     return resultStringBuilder.toString()
                 }
             }
         }
-
     }
 }
 
-fun randomNanoId() = randomNanoId(generator, alphabet, defaultSize)
+private fun calculateStep(mask: Int, size: Int, chars: CharArray) =
+    ceil(1.6 * mask * size / chars.size).toInt()
+
+private fun calculateMask(chars: CharArray) =
+    (2 shl (floor(ln((chars.size - 1).toDouble()) / ln(2.0))).toInt()) - 1
+
+private fun validateArguments(chars: CharArray, size: Int) {
+    if (chars.isEmpty() || chars.size >= 256) throw IllegalArgumentException("chars must be in range (0..256)")
+    if (size <= 0) throw IllegalArgumentException("size must be more than zero")
+}
--- src/test/kotlin/org/knanoid/KnanoidKtTest.kt
+++ src/test/kotlin/org/knanoid/KnanoidKtTest.kt
@@ -1,7 +1,12 @@
 package org.knanoid
 
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.collections.HashSet
+import kotlin.math.abs
+
 
 internal class KnanoidKtTest {
 
@@ -13,5 +18,55 @@ internal class KnanoidKtTest {
             assertTrue(results.add(generated))
         }
     }
+
+    @Test
+    fun checkWrongSizeException() {
+        assertThrows(IllegalArgumentException::class.java) {
+            randomNanoId(0)
+        }
+
+        assertThrows(IllegalArgumentException::class.java) {
+            randomNanoId(-1)
+        }
+    }
+
+    @Test
+    fun checkAlphabetMustBeBetween0and256Exception() {
+        assertThrows(IllegalArgumentException::class.java) {
+            randomNanoId(alphabet = "".toCharArray())
+        }
+
+        assertThrows(IllegalArgumentException::class.java) {
+            val bigAlphabet = generateSequence { 'A' }.take(257).toList().toCharArray()
+            randomNanoId(alphabet = bigAlphabet)
+        }
+    }
+
+    @Test
+    fun checkWellDistributedProperty() {
+        val charAlphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray()
+        val charCounts: MutableMap<String, Long> = HashMap()
+
+        repeat(100_000) {
+            val id = randomNanoId(20, charAlphabet)
+
+            for (element in id) {
+                val value = element.toString()
+                val charCount = charCounts[value]
+                if (charCount == null) {
+                    charCounts[value] = 1L
+                } else {
+                    charCounts[value] = charCount + 1
+                }
+            }
+        }
+
+        for (charCount in charCounts.values) {
+            val distribution: Double = (charCount * charAlphabet.size / (charCount * 20)).toDouble()
+            assertTrue(almostEqual(distribution, 1.0, 0.05) || almostEqual(distribution, 0.05, 0.05))
+        }
+    }
+
+    private fun almostEqual(a: Double, b: Double, eps: Double) = abs(a - b) < eps
 
 }
